fb8a6aa18f9c89786cdd52ab9e79ea8daf9a81e7
Chirag Jani <59524734+ChiragJani1@users.noreply.github.com>
2021-12-28 17:01:20 +0530

    Redefined Schema Added some restriction

--- model/Product.js
+++ model/Product.js
@@ -5,11 +5,11 @@ let Schema = mongoose.Schema;
 
 let Product = new Schema({
     ProductId: {type:ObjectId,required: true, auto:true},
-    ProductName: String,
-    qtyPerUnit: Number,
-    unitPrice: Number,
+    ProductName: {type:String, required: true},
+    qtyPerUnit: {type:Number, required: true},
+    unitPrice: {type:Number, required: true},
     unitInStock: Number,
-    discontinued: Boolean,
+    discontinued: {type:Boolean, required: true},
     categoryId: Number
 
 })
